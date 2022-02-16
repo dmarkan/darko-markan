8f166ad873c74591a8ccae6cf4da4295f40b22bb
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-02-16 14:58:38 +0100

    aplikacije zavrsene, jos kontakt je ostao da se uradi

--- src/pages/Apps.js
+++ src/pages/Apps.js
@@ -2,11 +2,54 @@ import React from "react";
   
 const Apps = () => {
   return (
-    <div>
-      <h1>
-        Apps
-      </h1>
+    <div class="container aplikacije-margin">
+    <h1>Aplikacije</h1>
+    <p class="paragraph-txt">Sve aplikacije su napravljene pomoću Javascript programskog jezika i dostupne su na Google Play prodavnici.</p>
+    <div class="row text-center">
+      <div class="col-lg-4">
+        <img class="bd-placeholder-img rounded-circle" src="images/etn-icon.png" width="140" height="140" />
+        <h4>Escape The Night</h4>
+        <p>Probudio si se u mračnoj spavaćoj sobi. Kako si dospeo ovde? Možeš li pobeći od noći?</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wEscapeTheNight_14793758" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+         <img class="bd-placeholder-img rounded-circle" src="images/pesmice-icon.png" width="140" height="140" />
+        <h4>Pesmice</h4>
+        <p>Dečije pesmice na jednom mestu.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wPesmice_14881126" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="images/vreme-icon.png" width="140" height="140" />
+        <h4>Beograd Vreme</h4>
+        <p>Vremenska prognoza za Beograd.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wBeogradVreme_15038987" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="images/tuscan-icon.png" width="140" height="140" />
+        <h4>Tuscan</h4>
+        <p>Uživaj u svetu D&D-a iz drugačije perspektive.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wTuscan_15043636" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="images/baby-icon.png" width="140" height="140" />
+        <h4>Calm My Baby</h4>
+        <p>Beli šum smiruje i pomaže vašoj bebi da zaspi.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wCalmMyBaby_15083309" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="images/jamb-icon.png" width="140" height="140" />
+        <h4>Jamb</h4>
+        <p>Jamb društvena igra.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wJamb_15116370" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img class="bd-placeholder-img rounded-circle" src="images/geografija-icon.png" width="140" height="140" />
+        <h4>Zanimljiva Geografija</h4>
+        <p>Društvena igra Zanimljiva Geografija.</p>
+        <p><a class="btn btn-primary" href="https://play.google.com/store/apps/details?id=com.wZanimljivaGeografija_15158223" target="_blank"><i class="fab fa-google-play"></i> PREUZMI OVDE</a></p>
+      </div>
     </div>
+  </div>
   );
 };
   
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,10 +2,16 @@ import React from "react";
   
 const Contact = () => {
   return (
-    <div>
+    <div class="row mb-2 text-center d-flex flex-column justify-content-center align-items-center profile paragraph-txt about">
+    <div class="col-md-6">
+      <div class="col-auto">
       <h1>
-      Contact
+      Kontakt
       </h1>
+      <p class="paragraph-txt">Ako ste zainteresovani za saradnju možete me kontaktirati na mail:</p>
+      <p class="paragraph-txt">markandarko@yahoo.com</p>
+    </div>
+    </div>
     </div>
   );
 };
